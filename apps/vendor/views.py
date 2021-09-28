6c12b08261049b323501a52b5c65e21efa66b4cd
AnastasiiaK120 <anastasiiakudriavtseva4@gmail.com>
2021-09-28 13:00:03 -0400

    Add show list of products

--- apps/vendor/views.py
+++ apps/vendor/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
 from .models import Vendor
+from apps.product.models import Product
 
 def become_vendor(request):
     if request.method == 'POST':
@@ -26,5 +27,6 @@ def become_vendor(request):
 @login_required
 def vendor_admin(request):
     vendor = request.user.vendor
+    products = vendor.products.all()
 
-    return render(request, 'vendor/vendor_admin.html', {'vendor': vendor})
+    return render(request, 'vendor/vendor_admin.html', {'vendor': vendor, 'products': products})
