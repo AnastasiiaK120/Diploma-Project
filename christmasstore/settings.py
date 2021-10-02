507df2a21eefecf82802ea1f87de0915f2cba159
Ubuntu <ubuntu@ip-172-31-18-195.ec2.internal>
2021-10-02 20:16:43 +0000

    Allow host

--- christmasstore/settings.py
+++ christmasstore/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 STRIPE_PUB_KEY = os.getenv('STRIPE_PUB_KEY')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
