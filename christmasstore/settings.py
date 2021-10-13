f9f981634926f2262798aa23654d74a651ab24b1
Anastasiia Kudriavtseva <82476413+AnastasiiaK120@users.noreply.github.com>
2021-10-13 09:58:14 -0400

    Update settings.py

--- christmasstore/settings.py
+++ christmasstore/settings.py
@@ -43,7 +43,7 @@ CART_SESSION_ID = 'cart'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'anadjango8@gmail.com'
-EMAIL_HOST_PASSWORD = 'hA8WMjfJcP4X4Zd'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
